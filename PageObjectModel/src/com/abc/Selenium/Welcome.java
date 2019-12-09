f7e2fcfaf7144fae135383a0d8bdde77c5cfcd63
ABC <ABC@DESKTOP-CI00JE3>
2019-12-09 02:40:39 -0800

    commit one

---
+++ PageObjectModel/src/com/abc/Selenium/Welcome.java
@@ -0,0 +1,25 @@
+package com.abc.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Welcome {
+	WebDriver driver;
+	@FindBy(linkText="My Account")
+	WebElement myacct;
+		
+		public Welcome(WebDriver driver)
+		{
+			this.driver=driver;
+			PageFactory.initElements(driver, this);
+		}
+		public void clickOnMyAccount()
+		{
+			myacct.click();
+		}
+
+
+
+}
---
+++ PageObjectModel/src/com/abc/Utilities/MagentoTest.java
@@ -0,0 +1,39 @@
+package com.abc.Utilities;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.abc.Selenium.Login;
+import com.abc.Selenium.Main;
+import com.abc.Selenium.Welcome;
+
+public class MagentoTest {
+
+	public static void main(String[] args) {
+		String url ="https://www.magento.com";
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+		Welcome w=new Welcome(driver);
+		w.clickOnMyAccount();
+		Login l = new Login(driver);
+		l.enterEmail();
+		l.enterpassword();
+		l.clickOnLogin();
+		Main m = new Main(driver);
+		m.clickOnLogout();
+		driver.close();
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
